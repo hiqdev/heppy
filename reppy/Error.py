c0f7a0a9d0548c9395d02b0d67bf372d1f570c2f
Andrii Vasyliev <sol@hiqdev.com>
2016-02-12 17:23:29 +0000

    + Error module

---
+++ reppy/Error.py
@@ -0,0 +1,20 @@
+
+class Error(Exception):
+    def __init__(self, message, data = {}):
+        self.message = message
+        self.data = data
+
+    def __str__(self):
+        return self.message + ": " + repr(self.data)
+
+    @staticmethod
+    def die(code, error, message = None):
+        if isinstance(error, Exception):
+            e = error
+            error = '{0}.{1}'.format(type(e).__module__, type(e).__name__)
+            message = str(e)
+        print 'Error: ' + error
+        if message:
+            print message
+        exit(code)
+
