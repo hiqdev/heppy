68a02790ae8c90f6afcaebe830d6800dd7437ed7
Andrii Vasyliev <sol@hiqdev.com>
2016-03-10 09:47:17 +0000

    + secDNS:keyData

--- reppy/modules/secDNS.py
+++ reppy/modules/secDNS.py
@@ -11,12 +11,35 @@ class secDNS(Module):
 ### REQUEST rendering
 
     def render_create(self, request):
-        ext = self.render_extension_fields(request, 'create', {'maxSigLife': {}})
-        data = request.sub(ext, 'secDNS:dsData')
+        ext = self.render_extension(request, 'create')
+        self.render_allData(request, ext)
+
+    def render_allData(self, request, parent):
+        if request.get('maxSigLife'):
+            request.sub(parent, 'secDNS:maxSigLife', {}, request.get('maxSigLife'))
+        if request.get('digest'):
+            self.render_dsData(request, parent)
+        else:
+            self.render_keyData(request, parent)
+
+    def render_dsData(self, request, parent):
+        data = request.sub(parent, 'secDNS:dsData')
         request.subfields(data, OrderedDict([
             ('keyTag', {}),
-            ('alg', {}),
+            ('alg', 'digestAlg'),
             ('digestType', {}),
             ('digest', {}),
+        ]))
+        self.render_keyData(request, data)
+
+    def render_keyData(self, request, parent):
+        if not request.get('pubKey'):
+            return
+        data = request.sub(parent, 'secDNS:keyData')
+        request.subfields(data, OrderedDict([
+            ('flags', {}),
+            ('protocol', {}),
+            ('alg', 'keyAlg'),
+            ('pubKey', {}),
         ]))
 
