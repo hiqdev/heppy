f7eb934260b09233ad3e2255c2db962991116f14
Andrii Vasyliev <sol@hiqdev.com>
2016-03-14 16:05:13 +0000

    * Client: added lazy connect and auto reconnect

--- heppy/Client.py
+++ heppy/Client.py
@@ -4,16 +4,30 @@ from heppy import Net
 
 class Client:
     def __init__(self, address):
+        self.socket  = None
+        self.address = address
+
+    def _connect(self):
         self.socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         self.socket.settimeout(0.01)
-        self.socket.connect(address)
+        self.socket.connect(self.address)
         self.socket.settimeout(None)
 
+    def connect(self):
+        if self.socket is None:
+            self._connect()
+
+    def disconnect(self):
+        self.socket = None
+
     def write(self, data):
+        self.connect()
         Net.write(self.socket, data)
 
     def read(self):
-        return Net.read(self.socket)
+        res = Net.read(self.socket)
+        self.disconnect()
+        return res
 
     def request(self, data):
         self.write(data)
@@ -23,6 +37,7 @@ class Client:
     def try_connect(address):
         try:
             client = Client(address)
+            client.connect()
             return True
         except:
             return False
