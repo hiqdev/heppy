f5506e6e3d799011d567d3cdb4971de86e01a03f
Andrii Vasyliev <sol@hiqdev.com>
2018-09-28 13:34:26 +0000

    Added `assertResponse()` in modules TestCase

--- tests/modules/TestCase.py
+++ tests/modules/TestCase.py
@@ -2,11 +2,20 @@
 
 import unittest
 from heppy.Request import Request
+from heppy.Response import Response
 
 
 class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.maxDiff = None
+
     def assertRequest(self, query, data):
         request = Request.buildFromDict(data)
         test = Request.prettifyxml(str(request))
 
         self.assertEqual(query.strip(), test.strip())
+
+    def assertResponse(self, data, reply):
+        response = Response.parsexml(reply)
+        self.assertDictEqual(data, response.data)
+
