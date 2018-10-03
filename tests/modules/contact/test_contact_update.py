fac411d543ae344dcc576841f38e502bef49faee
MaksimGayduk <maksim.gayduk@gmail.com>
2018-10-03 11:11:00 +0300

    contact:update done and covered with tests

--- tests/modules/contact/test_contact_update.py
+++ tests/modules/contact/test_contact_update.py
@@ -43,8 +43,8 @@ class TestContactCheck(TestCase):
             'id':       'sh8013',
             'add': {
                 'statuses': {
-                    'clientHold': 'Payment overdue.',
-                    'clientUpdateProhibited': None
+                    'clientHold':               'Payment overdue.',
+                    'clientUpdateProhibited':   None
                 }
             },
             'clTRID':   'XXXX-11',
@@ -70,8 +70,8 @@ class TestContactCheck(TestCase):
             'id':       'sh8013',
             'rem': {
                 'statuses': {
-                    'clientHold': 'Payment overdue.',
-                    'clientUpdateProhibited': None
+                    'clientHold':               'Payment overdue.',
+                    'clientUpdateProhibited':   None
                 }
             },
             'clTRID':   'XXXX-11',
@@ -126,6 +126,77 @@ class TestContactCheck(TestCase):
                 'pw':       '2fooBAR',
             },
             'clTRID':   'XXXX-11',
+        })
+
+    def test_render_contact_update_request(self):
+        self.assertRequest('''<?xml version="1.0" ?>
+<epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
+    <command>
+        <update>
+            <contact:update xmlns:contact="urn:ietf:params:xml:ns:contact-1.0">
+                <contact:id>sh8013</contact:id>
+                <contact:add>
+                    <contact:status s="clientHold">Payment overdue.</contact:status>
+                    <contact:status s="clientUpdateProhibited"/>
+                </contact:add>
+                <contact:rem>
+                    <contact:status s="clientHold">Payment overdue.</contact:status>
+                    <contact:status s="clientUpdateProhibited"/>
+                </contact:rem>
+                <contact:chg>
+                    <contact:postalInfo type="int">
+                        <contact:name>John Doe</contact:name>
+                        <contact:org>Example Inc.</contact:org>
+                        <contact:addr>
+                            <contact:street>123 Example Dr.</contact:street>
+                            <contact:street>Suite 100</contact:street>
+                            <contact:city>Dulles</contact:city>
+                            <contact:sp>VA</contact:sp>
+                            <contact:pc>20166-6503</contact:pc>
+                            <contact:cc>US</contact:cc>
+                        </contact:addr>
+                    </contact:postalInfo>
+                    <contact:voice>+1.7035555555</contact:voice>
+                    <contact:fax>+1.7035555556</contact:fax>
+                    <contact:email>jdoe@example.com</contact:email>
+                    <contact:authInfo>
+                        <contact:pw>2fooBAR</contact:pw>
+                    </contact:authInfo>
+                </contact:chg>
+            </contact:update>
+        </update>
+        <clTRID>XXXX-11</clTRID>
+    </command>
+</epp>''', {
+            'command': 'contact:update',
+            'id': 'sh8013',
+            'add': {
+                'statuses': {
+                    'clientHold':               'Payment overdue.',
+                    'clientUpdateProhibited':   None
+                }
+            },
+            'rem': {
+                'statuses': {
+                    'clientHold':               'Payment overdue.',
+                    'clientUpdateProhibited':   None
+                }
+            },
+            'chg': {
+                'name':     'John Doe',
+                'org':      'Example Inc.',
+                'city':     'Dulles',
+                'street1':  '123 Example Dr.',
+                'street2':  'Suite 100',
+                'sp':       'VA',
+                'pc':       '20166-6503',
+                'cc':       'US',
+                'voice':    '+1.7035555555',
+                'fax':      '+1.7035555556',
+                'email':    'jdoe@example.com',
+                'pw':       '2fooBAR',
+            },
+            'clTRID': 'XXXX-11',
         })
 
 
