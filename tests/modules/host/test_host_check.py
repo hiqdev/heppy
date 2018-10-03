2e04735d4c766ca22191419c1c0054e7695405d8
MaksimGayduk <maksim.gayduk@gmail.com>
2018-10-03 11:45:02 +0300

    Created host:check test

---
+++ tests/modules/host/test_host_check.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import unittest
+from ..TestCase import TestCase
+
+
+class TestHostCheck(TestCase):
+
+    def test_render_contact_check_request(self):
+        self.assertRequest('''<?xml version="1.0" ?>
+<epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
+    <command>
+        <check>
+            <host:check xmlns:host="urn:ietf:params:xml:ns:host-1.0">
+                <host:name>ns1.example1.com</host:name>
+                <host:name>ns2.example2.com</host:name>
+                <host:name>ns3.example2.com</host:name>
+            </host:check>
+        </check>
+        <clTRID>XXXX-11</clTRID>
+    </command>
+</epp>''', {
+            'command': 'host:check',
+            'names': [
+                'ns1.example1.com',
+                'ns2.example2.com',
+                'ns3.example2.com',
+            ],
+            'clTRID': 'XXXX-11',
+        })
+
+
+if __name__ == '__main__':
+    unittest.main()
