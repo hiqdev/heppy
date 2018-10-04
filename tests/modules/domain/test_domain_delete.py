e9cd8c93c4ba8daa39fd57bf1501c33d84a91435
MaksimGayduk <maksim.gayduk@gmail.com>
2018-10-04 16:36:04 +0300

    Added test domain:delete

--- tests/modules/domain/test_domain_delete.py
+++ tests/modules/domain/test_domain_delete.py
@@ -23,6 +23,26 @@ class TestDomainDelete(TestCase):
             'clTRID':   'XXXX-11',
         })
 
+    def test_parse_domain_delete_response(self):
+        self.assertResponse({
+            'clTRID':       'XXXX-11',
+            'result_code':  '1000',
+            'result_lang':  'en-US',
+            'result_msg':   'Command completed successfully',
+            'svTRID':       'SRW-425500000011130577'
+        }, '''<?xml version="1.0" ?>
+<epp xmlns="urn:ietf:params:xml:ns:epp-1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd">
+    <response>
+        <result code="1000">
+            <msg lang="en-US">Command completed successfully</msg>
+        </result>
+        <trID>
+            <clTRID>XXXX-11</clTRID>
+            <svTRID>SRW-425500000011130577</svTRID>
+        </trID>
+    </response>
+</epp>
+        ''')
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
