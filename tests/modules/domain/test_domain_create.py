57817bdd530e9e2c6b91fc1866914434ad274323
MaksimGayduk <maksim.gayduk@gmail.com>
2018-10-04 16:26:16 +0300

    Added test domain:create

--- tests/modules/domain/test_domain_create.py
+++ tests/modules/domain/test_domain_create.py
@@ -65,6 +65,37 @@ class TestDomainCreate(TestCase):
             'clTRID':       'XXXX-11',
         })
 
+    def test_parse_domain_create_response(self):
+        self.assertResponse({
+            'clTRID':       'XXXX-11',
+            'crDate':       '2018-10-04T13:17:43.0Z',
+            'exDate':       '2020-10-04T13:17:43.0Z',
+            'name':         'silverfire.me',
+            'result_code':  '1000',
+            'result_lang':  'en-US',
+            'result_msg':   'Command completed successfully',
+            'svTRID':       'SRW-425500000011131514'
+        }, '''<?xml version="1.0" ?>
+<epp xmlns="urn:ietf:params:xml:ns:epp-1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd">
+    <response>
+        <result code="1000">
+            <msg lang="en-US">Command completed successfully</msg>
+        </result>
+        <resData>
+            <domain:creData xmlns:domain="urn:ietf:params:xml:ns:domain-1.0" xsi:schemaLocation="urn:ietf:params:xml:ns:domain-1.0 domain-1.0.xsd">
+                <domain:name>silverfire.me</domain:name>
+                <domain:crDate>2018-10-04T13:17:43.0Z</domain:crDate>
+                <domain:exDate>2020-10-04T13:17:43.0Z</domain:exDate>
+            </domain:creData>
+        </resData>
+        <trID>
+            <clTRID>XXXX-11</clTRID>
+            <svTRID>SRW-425500000011131514</svTRID>
+        </trID>
+    </response>
+</epp>
+        ''')
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
