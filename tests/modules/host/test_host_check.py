729517e71c3bd0c3fdf98aa9d00dd0f4fbcd8d1e
MaksimGayduk <maksim.gayduk@gmail.com>
2018-10-05 12:49:37 +0300

    Added test host:check response

--- tests/modules/host/test_host_check.py
+++ tests/modules/host/test_host_check.py
@@ -29,6 +29,45 @@ class TestHostCheck(TestCase):
             'clTRID':   'XXXX-11',
         })
 
+    def test_parse_host_check_response(self):
+        self.assertResponse({
+            'avails': {
+                'ns1.silverfire.me': '0',
+                'ns2.silverfire.me': '0',
+                'ns3.silverfire.me': '1'
+            },
+            'clTRID':       'XXXX-11',
+            'result_code':  '1000',
+            'result_lang':  'en-US',
+            'result_msg':   'Command completed successfully',
+            'svTRID':       'SRO-1538732574720'
+        }, '''<?xml version="1.0" ?>
+<epp xmlns="urn:ietf:params:xml:ns:epp-1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd">
+    <response>
+        <result code="1000">
+            <msg lang="en-US">Command completed successfully</msg>
+        </result>
+        <resData>
+            <host:chkData xmlns:host="urn:ietf:params:xml:ns:host-1.0" xsi:schemaLocation="urn:ietf:params:xml:ns:host-1.0 host-1.0.xsd">
+                <host:cd>
+                    <host:name avail="0">ns1.silverfire.me</host:name>
+                </host:cd>
+                <host:cd>
+                    <host:name avail="0">ns2.silverfire.me</host:name>
+                </host:cd>
+                <host:cd>
+                    <host:name avail="1">ns3.silverfire.me</host:name>
+                </host:cd>
+            </host:chkData>
+        </resData>
+        <trID>
+            <clTRID>XXXX-11</clTRID>
+            <svTRID>SRO-1538732574720</svTRID>
+        </trID>
+    </response>
+</epp>
+        ''')
+
 
 if __name__ == '__main__':
     unittest.main()
