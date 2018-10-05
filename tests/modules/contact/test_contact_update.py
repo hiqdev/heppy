731d3164200d8efddb3624d7da1c5c4cc24dc2e1
MaksimGayduk <maksim.gayduk@gmail.com>
2018-10-05 11:30:50 +0300

    Added tests contact:update response

--- tests/modules/contact/test_contact_update.py
+++ tests/modules/contact/test_contact_update.py
@@ -199,6 +199,53 @@ class TestContactCheck(TestCase):
             'clTRID': 'XXXX-11',
         })
 
+    def test_parse_contact_update_response_success(self):
+        self.assertResponse({
+            'clTRID':       'XXXX-11',
+            'result_code':  '1000',
+            'result_lang':  'en-US',
+            'result_msg':   'Command completed successfully',
+            'svTRID':       'SRW-425500000011139056'
+        }, '''<?xml version="1.0" ?>
+<epp xmlns="urn:ietf:params:xml:ns:epp-1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd">
+    <response>
+        <result code="1000">
+            <msg lang="en-US">Command completed successfully</msg>
+        </result>
+        <trID>
+            <clTRID>XXXX-11</clTRID>
+            <svTRID>SRW-425500000011139056</svTRID>
+        </trID>
+    </response>
+</epp>
+        ''')
+
+
+    def test_parse_contact_update_response_fail(self):
+        self.assertResponse({
+            'clTRID':           'XXXX-11',
+            'result_code':      '2004',
+            'result_lang':      'en-US',
+            'result_msg':       'Parameter value range error',
+            'result_reason':    '2004:Parameter value range error (contact-status: clientHold)',
+            'svTRID':           'SRW-425500000011139080'
+        }, '''<?xml version="1.0" ?>
+<epp xmlns="urn:ietf:params:xml:ns:epp-1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd">
+    <response>
+        <result code="2004">
+            <msg lang="en-US">Parameter value range error</msg>
+            <value xmlns:oxrs="urn:afilias:params:xml:ns:oxrs-1.1">
+                <oxrs:xcp>2004:Parameter value range error (contact-status: clientHold)</oxrs:xcp>
+            </value>
+        </result>
+        <trID>
+            <clTRID>XXXX-11</clTRID>
+            <svTRID>SRW-425500000011139080</svTRID>
+        </trID>
+    </response>
+</epp>
+        ''')
+
 
 if __name__ == '__main__':
     unittest.main()
