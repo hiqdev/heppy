0cb0435115fb8bc20c418522b9f59a4b8c2db002
MaksimGayduk <maksim.gayduk@gmail.com>
2018-10-05 15:21:31 +0300

    Added test host:update response

--- tests/modules/host/test_host_update.py
+++ tests/modules/host/test_host_update.py
@@ -175,6 +175,27 @@ class TestHostUpdate(TestCase):
             'clTRID': 'XXXX-11',
         })
 
+    def test_parse_host_update_response(self):
+        self.assertResponse({
+            'clTRID':       'XXXX-11',
+            'result_code':  '1000',
+            'result_lang':  'en-US',
+            'result_msg':   'Command completed successfully',
+            'svTRID':       'SRW-425500000011139938'
+        }, '''<?xml version="1.0" ?>
+<epp xmlns="urn:ietf:params:xml:ns:epp-1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd">
+    <response>
+        <result code="1000">
+            <msg lang="en-US">Command completed successfully</msg>
+        </result>
+        <trID>
+            <clTRID>XXXX-11</clTRID>
+            <svTRID>SRW-425500000011139938</svTRID>
+        </trID>
+    </response>
+</epp>
+        ''')
+
 
 if __name__ == '__main__':
     unittest.main()
