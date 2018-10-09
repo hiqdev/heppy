16cc0185487ea02c0cdeda1a7bd5957ebf65efd9
MaksimGayduk <maksim.gayduk@gmail.com>
2018-10-09 15:58:25 +0300

    Created fee:create request and covered with test

--- tests/modules/fee/test_fee_check.py
+++ tests/modules/fee/test_fee_check.py
@@ -4,9 +4,9 @@ import unittest
 from ..TestCase import TestCase
 
 
-class TestContactCheck(TestCase):
+class TestFeeCheck(TestCase):
 
-    def test_render_contact_check_request(self):
+    def test_render_fee_check_request(self):
         self.assertRequest('''<?xml version="1.0" ?>
 <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
     <command>
