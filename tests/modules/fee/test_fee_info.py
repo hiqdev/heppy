90eaad62500b2cf77e7615748bc5cb48be4086e0
MaksimGayduk <maksim.gayduk@gmail.com>
2018-10-09 15:39:54 +0300

    Created fee:info request and covered with test

---
+++ tests/modules/fee/test_fee_info.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import unittest
+from ..TestCase import TestCase
+
+
+class TestContactCheck(TestCase):
+
+    def test_render_contact_check_request(self):
+        self.assertRequest('''<?xml version="1.0" ?>
+<epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
+    <command>
+        <info>
+            <domain:info xmlns:domain="urn:ietf:params:xml:ns:domain-1.0">
+                <domain:name hosts="all">silverfire.me</domain:name>
+            </domain:info>
+        </info>
+        <extension>
+            <fee:info xmlns:fee="urn:ietf:params:xml:ns:fee-0.5">
+                <fee:currency>USD</fee:currency>
+                <fee:action phase="sunrise">create</fee:action>
+                <fee:period unit="y">1</fee:period>
+            </fee:info>
+        </extension>
+        <clTRID>XXXX-11</clTRID>
+    </command>
+</epp>
+''', {
+            'command': 'domain:info',
+            'name': 'silverfire.me',
+            'extensions': [
+                {
+                    'command': 'fee:info',
+                    'name': 'silverfire.me',
+                    'action': 'create',
+                    'currency': 'USD',
+                    'phase': 'sunrise',
+                    'period': 1
+                },
+            ],
+            'clTRID': 'XXXX-11',
+        })
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
