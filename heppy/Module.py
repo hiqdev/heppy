9dc35650b4ad0589cab95eef4db6fe1357e22597
MaksimGayduk <maksim.gayduk@gmail.com>
2018-10-10 17:32:46 +0300

    Ordered module class methods

--- heppy/Module.py
+++ heppy/Module.py
@@ -34,27 +34,19 @@ class Module:
 
 ### REQUEST rendering
 
-    def render_epp(self, request):
-        if request.epp is None:
-            request.epp = request.add_tag('epp', {'xmlns': request.get_module('epp').xmlns})
-        return request.epp
-
-    def render_clTRID(self, request, data):
-        clTRID = data.get('clTRID', 'AA-00')
-        if clTRID != 'NONE' and request.command is not None:
-            request.add_subtag(request.command, 'clTRID', text=clTRID)
-
-    def render_root_command(self, request, command, attrs={}):
-        if request.command is None:
-            epp = self.render_epp(request)
-            request.command = request.add_subtag(epp, 'command')
-        return request.add_subtag(request.command, command, attrs)
+    ## Command
 
     def render_header(self, request, parent, command, attrs={}, text=None):
         header_attrs = {'xmlns:' + self.name: self.xmlns}
         if attrs:
             header_attrs.update(attrs)
         return request.add_subtag(parent, self.name + ':' + command, header_attrs, text)
+
+    def render_root_command(self, request, command, attrs={}):
+        if request.command is None:
+            epp = self.render_epp(request)
+            request.command = request.add_subtag(epp, 'command')
+        return request.add_subtag(request.command, command, attrs)
 
     def render_command(self, request, command, attrs={}):
         command_tag = self.render_root_command(request, command, attrs)
@@ -65,19 +57,7 @@ class Module:
         request.add_subtags(command, fields)
         return command
 
-    def render_check_command(self, request, data, field):
-        command = self.render_command(request, 'check')
-        for name in data.get(field + 's'):
-            request.add_subtag(command, self.name + ':' + field, text=name)
-        return command
-
-    def render_auth_info(self, request, parent, pw='', attrs={}):
-        auth_info = request.add_subtag(parent, self.name + ':authInfo')
-        request.add_subtag(auth_info, self.name + ':pw', attrs, pw)
-
-    def render_statuses(self, request, parent, status_data):
-        for status, description in status_data.iteritems():
-            request.add_subtag(parent, self.name + ':status', {'s': status}, description)
+    ## Extension
 
     def render_root_extension(self, request):
         if request.extension is None:
@@ -92,3 +72,29 @@ class Module:
         extension = self.render_extension(request, command, attrs)
         request.add_subtags(extension, fields)
         return extension
+
+    ## Common methods of modules
+
+    def render_epp(self, request):
+        if request.epp is None:
+            request.epp = request.add_tag('epp', {'xmlns': request.get_module('epp').xmlns})
+        return request.epp
+
+    def render_clTRID(self, request, data):
+        clTRID = data.get('clTRID', 'AA-00')
+        if clTRID != 'NONE' and request.command is not None:
+            request.add_subtag(request.command, 'clTRID', text=clTRID)
+
+    def render_check_command(self, request, data, field):
+        command = self.render_command(request, 'check')
+        for name in data.get(field + 's'):
+            request.add_subtag(command, self.name + ':' + field, text=name)
+        return command
+
+    def render_auth_info(self, request, parent, pw='', attrs={}):
+        auth_info = request.add_subtag(parent, self.name + ':authInfo')
+        request.add_subtag(auth_info, self.name + ':pw', attrs, pw)
+
+    def render_statuses(self, request, parent, status_data):
+        for status, description in status_data.iteritems():
+            request.add_subtag(parent, self.name + ':status', {'s': status}, description)
