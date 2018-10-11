ed03cc3c2aa9394a1b240316852cd15cb52c0b4d
MaksimGayduk <maksim.gayduk@gmail.com>
2018-10-11 18:28:53 +0300

    Created fee:create parse response and covered with test

--- tests/modules/fee/test_fee_create.py
+++ tests/modules/fee/test_fee_create.py
@@ -37,27 +37,71 @@ class TestFeeCreate(TestCase):
     </command>
 </epp>
 ''', {
-            'command': 'domain:create',
-            'name': 'example.com',
-            'period': 2,
-            'registrant': 'jd1234',
+            'command':      'domain:create',
+            'name':         'example.com',
+            'period':       2,
+            'registrant':   'jd1234',
             'nss': [
                 'ns1.example.net',
                 'ns2.example.net'
             ],
-            'admin': 'sh8013',
-            'tech': 'sh8014',
-            'billing': 'sh8015',
-            'pw': '2fooBAR',
+            'admin':        'sh8013',
+            'tech':         'sh8014',
+            'billing':      'sh8015',
+            'pw':           '2fooBAR',
             'extensions': [
                 {
-                    'command': 'fee:create',
+                    'command':  'fee:create',
                     'currency': 'USD',
-                    'fee': 42.42
+                    'fee':      42.42
                 },
             ],
-            'clTRID': 'XXXX-11',
+            'clTRID':       'XXXX-11',
         })
+
+    def test_parse_fee_create_response(self):
+        self.assertResponse({
+            'clTRID':       'ABC-12345',
+            'crDate':       '1999-04-03T22:00:00.0Z',
+            'exDate':       '2001-04-03T22:00:00.0Z',
+            'name':         'example.com',
+            'result_code':  '1000',
+            'result_msg':   'Command completed successfully',
+            'svTRID':       '54321-XYZ',
+            'extensions': [
+                {
+                    'command':  'fee:create',
+                    'currency': 'USD',
+                    'fee':      '5.00'
+                }
+            ],
+        }, '''<?xml version="1.0" encoding="utf-8" standalone="no"?>
+<epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
+    <response>
+        <result code="1000">
+            <msg>Command completed successfully</msg>
+        </result>
+        <resData>
+            <domain:creData
+                xmlns:domain="urn:ietf:params:xml:ns:domain-1.0">
+                <domain:name>example.com</domain:name>
+                <domain:crDate>1999-04-03T22:00:00.0Z</domain:crDate>
+                <domain:exDate>2001-04-03T22:00:00.0Z</domain:exDate>
+            </domain:creData>
+        </resData>
+        <extension>
+            <fee:creData xmlns:fee="urn:ietf:params:xml:ns:fee-0.5">
+                <fee:currency>USD</fee:currency>
+                <fee:fee>5.00</fee:fee>
+            </fee:creData>
+        </extension>
+        <trID>
+            <clTRID>ABC-12345</clTRID>
+            <svTRID>54321-XYZ</svTRID>
+        </trID>
+    </response>
+</epp>
+        ''')
 
 
 if __name__ == '__main__':
