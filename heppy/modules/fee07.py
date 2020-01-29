c1b983badfc4827b0f4fbf1e559a041399e48b5c
Yurii Myronchuk <bladeroot@gmail.com>
2020-01-29 15:05:47 +0000

    change: use  for fee

--- heppy/modules/fee07.py
+++ heppy/modules/fee07.py
@@ -1,10 +1,4 @@
 from fee import fee
 
 class fee07(fee):
-<<<<<<< HEAD
     pass
-=======
-    def __init__(self, xmlns):
-        fee.__init__(self, xmlns)
-
->>>>>>> change constructor
--- heppy/modules/fee11.py
+++ heppy/modules/fee11.py
@@ -1,13 +1,6 @@
 from fee import fee
 
 class fee11(fee):
-<<<<<<< HEAD
-=======
-    def __init__(self, xmlns):
-        fee.__init__(self, xmlns)
-
-### REQUEST rendering
->>>>>>> change constructor
     def render_check(self, request, data):
         ext = self.render_extension(request, 'check')
         request.add_subtag(ext, 'fee:currency',  {},             data.get('currency'))
