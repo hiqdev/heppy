27a1f1940eb1b5dc7ccde6456d4544113272252a
Yurii Myronchuk <bladeroot@gmail.com>
2021-11-15 07:59:00 +0000

    improvements in code; fix info in UA zones; add attributes to keysys

--- heppy/modules/domain_hm.py
+++ heppy/modules/domain_hm.py
@@ -1,8 +1,16 @@
 from domain import domain
 
+from ..TagData import TagData
+
 class domain_hm(domain):
     CONTACT_TYPES = (
         'admin',
         'tech',
     )
 
+    def render_info(self, request, data):
+        command = self.render_command_with_fields(request, 'info', [
+            TagData('name', data.get('name'), {})
+        ])
+        if 'pw' in data:
+            self.render_auth_info(request, command, data['pw'])
