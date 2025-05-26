33a51fc76e996a71b58c513db789a80312f62e98
Yurii Myronchuk <bladeroot@gmail.com>
2025-05-26 20:06:27 +0200

    csfix

--- heppy/TagData.py
+++ heppy/TagData.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 class TagData:
-    def __init__(self, name, value=None, attrs={}):
+    def __init__(self, name, value=None, attrs=None):
         self.name = name
         self.value = value
-        self.attrs = self.filter_attrs(attrs)
+        self.attrs = {} if attrs is None else self.filter_attrs(attrs)
 
     def filter_attrs(self, attrs):
         return {attr: value for attr, value in attrs.items() if value}
